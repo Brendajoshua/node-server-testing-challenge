3a3a6bcddb355b1d818401f22b1efa9a16bf1987
Brenda Joshua <brendajosh.bj@gmail.com>
2019-11-14 16:29:03 +0300

    added middlewares

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const logger = require('logger');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
+server.use(logger());
 server.use(express.json());
 
 server.get('/', (req, res) => {
